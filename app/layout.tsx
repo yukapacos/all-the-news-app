84cff9c085f4f1c79a593bb211ad4056698ccc31
yuka <yuka.morinaga28@gmail.com>
2025-05-26 16:16:00 +0900

    chore - update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,32 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "My news feed",
-  description: "My news feed",
+const metadata: Metadata = {
+  title: "Simple News Feed",
+  description: "シンプルなニュースフィードのデモです",
+  openGraph: {
+    title: "Simple News Feed",
+    description: "シンプルなニュースフィードのデモです",
+    url: "https://news-feed.yukaten.com/",
+    siteName: "Simple News Feed",
+    images: [
+      {
+        url: "https://news-feed.yukaten.com/favicon.ico",
+        width: 1200,
+        height: 630,
+        alt: "Simple News Feed プレビュー画像",
+      },
+    ],
+    locale: "ja_JP",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Simple News Feed",
+    description: "シンプルなニュースフィードのデモです",
+    images: ["https://news-feed.yukaten.com/favicon.ico"],
+    // site: "",
+  },
 };
 
 export default function RootLayout({
